97b87ab0299cb5dffd8c86cba7b9a8624d972631
Anuj Dayal <anuj@near.co>
2022-08-17 12:49:10 +0530

    Fixing the issue Problems

--- src/com/anuj/Sorting/BubbleSorting.java
+++ src/com/anuj/Sorting/BubbleSorting.java
@@ -18,7 +18,8 @@ public class BubbleSorting {
 
     public static void main(String[] args) {
 //        int[] arr = {3, 1, -4, 3, 9, 13, 14, 20, 16, 15};
-        int[] arr = {-4, 1, 3, 3, 9, 13, 14, 15, 16, 20};
+        int[] arr = {2,5,1,32,8,3,-3,6,9,13};
+
         bubbleSort(arr);
         System.out.println(Arrays.toString(arr));
     }
@@ -26,19 +27,18 @@ public class BubbleSorting {
     public static void bubbleSort(int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
             boolean swapped = false;
-            for (int j = i + 1; j < arr.length - i - 1; j++) {
-                if (arr[i] > arr[j]) {
-                    arr[i] = arr[j] + arr[j] - (arr[j] = arr[i]);
-//                    int temp = arr[j];
-//                    arr[j]=arr[i];
-//                    arr[i]=temp;
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j+1]) {
+//                    arr[i] = arr[j] + arr[j] - (arr[j] = arr[i]);
+                    int temp = arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
                     swapped = true;
                 }
-                if (!swapped) {
-                    break;
                 }
+            if (!swapped) {
+                break;
             }
         }
-
     }
 }
--- src/com/anuj/Sorting/SelectionSort.java
+++ src/com/anuj/Sorting/SelectionSort.java
@@ -20,11 +20,11 @@ public class SelectionSort {
 
     public static void main(String[] args){
         int[] arr = {-5, -6, 0, 5, 3, -4, 13, 7, 23, 17, 8};
-        selectionSort(arr);
+        selectionSortUsingMax(arr);
         System.out.println(Arrays.toString(arr));
     }
 
-    public static void selectionSort(int[] arr) {
+    public static void selectionSortUsingMax(int[] arr) {
         for( int i =0; i<arr.length; i++){
             int lastIndex = arr.length-i-1;
            int maxIndex =  findMaxIndex(arr, lastIndex);
@@ -40,5 +40,19 @@ public class SelectionSort {
             }
         }
         return max;
+    }
+
+    public static void selectionSortUsingMin(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            int min_idx = i;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[min_idx] > nums[j]) {
+                    min_idx = j;
+                }
+                int temp = nums[min_idx];
+                nums[min_idx] = nums[i];
+                nums[i] = temp;
+            }
+        }
     }
 }
